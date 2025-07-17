a61c3c480ec8cbea05a021e0e70d468605ace508
Alsabbagh Louai <144901821+louals@users.noreply.github.com>
2025-07-16 20:47:30 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.routes import chatbot
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Chatbot Service for QuizGame")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or "*" to allow all for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(chatbot.router, prefix="/chatbot", tags=["Chatbot"])
